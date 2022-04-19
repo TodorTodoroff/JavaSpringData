85d018d0d1fd35ae2298d8e0c155db411f3879d6
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-04-19 09:40:15 +0300

    Add files via upload

---
+++ SpringDataUsers/src/main/java/com/example/springdatausers/SpringDataUsersApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatausers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataUsersApplication.class, args);
+    }
+
+}
---
+++ SpringDataUsers/src/test/java/com/example/springdatausers/SpringDataUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatausers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
